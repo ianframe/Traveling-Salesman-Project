fac962fcad8c2ad3659e2002dcf8319d5c2b24b2
ianframe <ian.frame@hies.org>
2017-07-13 09:05:21 -0400

    initial upload

---
+++ Tour.java
@@ -0,0 +1,30 @@
+public class Tour {
+
+    // creates an empty tour
+    public Tour()                                    
+    {}
+    // creates the 4-point tour a->b->c->d->a (for debugging)
+    public Tour(Point a, Point b, Point c, Point d)  
+    {}
+    // returns the number of points in this tour
+    public int size()
+    {return 0;}
+    // returns the length of this tour
+    public double length()
+    {return 0;}
+    // returns a string representation of this tour
+    public String toString()
+    {return null;}
+    // draws this tour to standard drawing
+    public void draw()
+    {}
+    // inserts p using the nearest neighbor heuristic
+    public void insertNearest(Point p)
+    {}
+    // inserts p using the smallest increase heuristic
+    public void insertSmallest(Point p)                   
+    {}
+   //tests this class by directly calling all constructors and methods
+    public static void main(String[] args)
+    {}
+}
