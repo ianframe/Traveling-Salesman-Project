0cd67487e40c95d01d3192c915768ea3ac2e5226
ianframe <ian.frame@hies.org>
2017-07-13 09:55:50 -0400

    Update Tour.java
    made distinction between size and length of tour.

--- Tour.java
+++ Tour.java
@@ -9,22 +9,28 @@ public class Tour {
     // returns the number of points in this tour
     public int size()
     {return 0;}
-    // returns the length of this tour
+    
+    // returns the length (total distance) of this tour
     public double length()
-    {return 0;}
+    {return 0.0;}
+    
     // returns a string representation of this tour
     public String toString()
     {return null;}
+    
     // draws this tour to standard drawing
     public void draw()
     {}
+    
     // inserts p using the nearest neighbor heuristic
     public void insertNearest(Point p)
     {}
+    
     // inserts p using the smallest increase heuristic
     public void insertSmallest(Point p)                   
     {}
-   //tests this class by directly calling all constructors and methods
+    
+    //tests this class by directly calling all constructors and methods
     public static void main(String[] args)
     {}
 }
