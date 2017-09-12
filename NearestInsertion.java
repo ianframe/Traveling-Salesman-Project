1e40fc64962fea383acd9ed5edb78903d4056086
ianframe <ian.frame@hies.org>
2017-09-12 16:00:10 -0400

    Squashed commit of the following:
    commit c05fb2fb5a6c52a2d6695ecf8904dd51353c4ee1
    Author: ianframe <ian.frame@hies.org>
    Date:   Tue Sep 12 15:55:08 2017 -0400
    
        add use of scanner for runners
    
    commit bbca624cc4cf89f9dcb7c3a32c4818905bdeeb1f
    Author: ianframe <ian.frame@hies.org>
    Date:   Sat Sep 9 10:57:03 2017 -0400
    
        Delete Queue.java
    
    commit 0cd67487e40c95d01d3192c915768ea3ac2e5226
    Author: ianframe <ian.frame@hies.org>
    Date:   Thu Jul 13 09:55:50 2017 -0400
    
        Update Tour.java
    
        made distinction between size and length of tour.
    
    commit a3cff4f31f102f3001ff855e934d668d4998737b
    Author: ianframe <ian.frame@hies.org>
    Date:   Thu Jul 13 09:29:24 2017 -0400
    
        Update README.md
    
    commit fac962fcad8c2ad3659e2002dcf8319d5c2b24b2
    Author: ianframe <ian.frame@hies.org>
    Date:   Thu Jul 13 09:05:21 2017 -0400
    
        initial upload

---
+++ NearestInsertion.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException; 
+
+/*************************************************************************
+ *  YOU DO NOT NEED TO MODIFY THIS FILE
+ *
+ *  Dependencies: java.util.Scanner StdDraw.java
+ *
+ *  Run nearest neighbor insertion heuristic for traveling
+ *  salesperson problemand plot results. Pass the name of the text file
+ *  located inside ./input as a command line argument.
+ *
+ *  % java NearestInsertion tsp1000.txt
+ *
+ *************************************************************************/
+
+public class NearestInsertion {
+
+    public static void main(String[] args) throws FileNotFoundException{
+
+        File f = new File("./input/" + args[0]);
+        Scanner s = new Scanner(f);
+
+        // get dimensions
+        String[] dimensions = s.nextLine().split(" ");
+        int width = Integer.parseInt(dimensions[0]);
+        int height = Integer.parseInt(dimensions[1]);
+        int border = 20;
+        StdDraw.setCanvasSize(width, height + border);
+        StdDraw.setXscale(0, width);
+        StdDraw.setYscale(-border, height);
+
+        // turn on animation mode
+        StdDraw.enableDoubleBuffering();
+
+        // run smallest insertion heuristic
+        Tour tour = new Tour();
+        s.reset();
+        while (s.hasNextDouble()) {
+            double x = Double.parseDouble(s.next());
+            double y = Double.parseDouble(s.next());
+            Point p = new Point(x, y);
+            tour.insertNearest(p);
+
+            // uncomment the lines below to animate
+            // StdDraw.clear();
+            // tour.draw();
+            // StdDraw.textLeft(20, 0, "length = " + tour.length());
+            // StdDraw.show();
+            // StdDraw.pause(50);
+        }
+
+        // draw to standard draw
+        tour.draw();
+        StdDraw.show();
+        
+        // print tour to standard output
+        StdOut.println(tour);
+        StdOut.printf("Tour length = %.4f\n", tour.length());
+        StdOut.printf("Number of points = %d\n", tour.size());
+    }
+
+}
